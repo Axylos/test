65e3180f9379c2f0b3d178bc4fef81c45f91de1b
drake <=>
2018-08-23 09:59:05 -0400

    port env stuff

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bookRouter = require('./routes/Books');
 const authorRouter = require('./routes/Authors');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'))
 
